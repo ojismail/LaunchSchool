b570500b04aca0210eca9695377533f6c17f3cb7
Ozair Ismail <ojismail@gmail.com>
2023-09-07 21:00:44 +0100

    Calculator.rb

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,28 @@
+puts "Hello please enter two numbers"
+
+number_1 = gets.chomp.to_f
+
+number_2 = gets.chomp.to_f
+
+puts "Would you like to add, subtract, multiply or divide?"
+
+operation = gets.chomp 
+
+if operation == "add"
+    result = number_1 + number_2
+elsif operation == "subtract"
+    result = number_1 - number_2
+elsif operation == "multiply"
+    result = number_1 * number_2
+elsif operation == "divide"
+    if number_2 == 0
+        puts "Cannot divide by 0"
+    else 
+        result = number_1 / number_2
+    end 
+else
+    "Invalid" 
+end 
+
+puts result 
+
