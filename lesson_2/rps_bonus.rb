591a945af6899bb0f955ed3e8dba42892450792d
Ozair Ismail <ojismail@gmail.com>
2023-09-21 12:23:41 +0100

    Lesson 3 begun

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -8,6 +8,14 @@ WINNING_COMBINATIONS = {
   'spock' => ['scissors', 'rock']
 }
 
+CHOICE_SHORTHAND = {
+  'r' => 'rock',
+  'p' => 'paper',
+  'sc' => 'scissors',
+  'sp' => 'spock',
+  'l' => 'lizard'
+}
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end 
@@ -15,20 +23,32 @@ end
 def display_results(player, computer)
   if WINNING_COMBINATIONS[player].include?(computer)
     prompt("You win")
+    return "player"
   elsif WINNING_COMBINATIONS[computer].include?(player)
     prompt("Computer wins")
+    return "computer"
   else
     prompt("It's a draw")
+    return "draw"
   end 
 end
+
+player_score = 0
+computer_score = 0
 
 loop do 
 choice = ''
 
+
 loop do 
   prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+  prompt("Shorthand: r = rock, p = paper, sc = scissors, sp = spock, l = lizard")
   choice = Kernel.gets().chomp()
 
+  if CHOICE_SHORTHAND.key?(choice)
+    choice = CHOICE_SHORTHAND[choice]
+  end 
+  
   if VALID_CHOICES.include?(choice)
     break 
   else 
@@ -40,7 +60,23 @@ computer_choice = VALID_CHOICES.sample
 
 prompt("You chose: #{choice}; Computer chose #{computer_choice}")
 
-display_results(choice, computer_choice)
+result = display_results(choice, computer_choice)
+
+if result == "player"
+  player_score += 1
+elsif result == "computer"
+  computer_score += 1
+end
+
+prompt("Your score: #{player_score}. Computer score: #{computer_score}.")
+
+if player_score == 3
+  prompt("Congratulations! You're the grand winner!")
+  break
+elsif computer_score == 3
+  prompt("Sorry! The computer is the grand winner!")
+  break
+end
 
 prompt("Do you want to play again?")
 answer = Kernel.gets().chomp()
